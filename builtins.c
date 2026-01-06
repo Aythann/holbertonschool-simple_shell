4cb2a5e0b7853254af89b93018185527eb0113a2
Aythann <aythancristovao@gmail.com>
2026-01-06 10:44:08 +0100

    Implement cd builtin to change current working directory

--- builtins.c
+++ builtins.c
@@ -18,3 +18,31 @@ void exitt(char **arv)
 	}
 }
 
+/**
+ * cd - changes the current directory
+ * @arv: arguments
+ *
+ * Return: void
+ */
+void cd(char **arv)
+{
+	char *dir;
+
+	if (arv[1] == NULL)
+	{
+		dir = _getenv("HOME");
+		if (dir == NULL)
+			dir = "/";
+	}
+	else
+	{
+		dir = arv[1];
+	}
+
+	if (chdir(dir) != 0)
+	{
+		_puts("hsh: cd: ");
+		_puts(dir);
+		_puts(": No such file or directory\n");
+	}
+}
