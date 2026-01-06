57e24bcb4ab7d2548dc25b1b0baa2be389a54447
Aythann <aythancristovao@gmail.com>
2026-01-06 11:06:10 +0100

    Add custom strncmp function for partial string comparison

--- string_utils.c
+++ string_utils.c
@@ -24,3 +24,30 @@ int _strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
+/**
+ * _strncmp - compares two strings up to n characters
+ * @s1: first string
+ * @s2: second string
+ * @n: number of characters to compare
+ *
+ * Return: 0 if identical, negative or positive otherwise
+ */
+int _strncmp(char *s1, const char *s2, int n)
+{
+	int i = 0;
+
+	if (s1 == NULL || s2 == NULL || n <= 0)
+		return (1);
+
+	while (i < n && s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	if (i == n)
+		return (0);
+
+	return (s1[i] - s2[i]);
+}
