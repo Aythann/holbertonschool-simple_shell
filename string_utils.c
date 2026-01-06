c950413c490b485c983341a6beff3111bb731387
Aythann <aythancristovao@gmail.com>
2026-01-06 10:36:53 +0100

    Implement custom strcmp function for string comparison

---
+++ string_utils.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if identical, negative or positive otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (1);
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
+
