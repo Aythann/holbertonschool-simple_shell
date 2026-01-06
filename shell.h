700448eeb565c30c9ff31eeba147b92e31d9675c
Aythann <aythancristovao@gmail.com>
2026-01-06 11:13:21 +0100

    Update shell header with new function prototypes for simple shell

--- shell.h
+++ shell.h
@@ -20,12 +20,19 @@ int _atoi(char *s);
 
 /* ========== string_utils.c ========== */
 int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, const char *s2, int n);
+char *_strcat(char *dest, char *src);
+char *concat_all(char *name, char *sep, char *value);
 
 /* ========== parser.c ========== */
 char **splitstring(char *str, const char *delim);
+void freearv(char **arv);
 
 /* ========== env.c ========== */
 char *_getenv(const char *name);
+void env(char **arv);
+void _setenv(char **arv);
+void _unsetenv(char **arv);
 
 /**
  * struct list_path - linked list of PATH directories
@@ -40,11 +47,19 @@ typedef struct list_path
 
 /* ========== path.c ========== */
 list_path *add_node_end(list_path **head, char *str);
+list_path *linkpath(char *path);
+char *_which(char *filename, list_path *head);
+void free_list(list_path *head);
 
 /* ========== execute.c ========== */
+int exec_direct(char **argv);
+int exec_path(char **argv);
+int execute(char **argv);
 int is_builtin(char **argv);
+void run_builtin(char **argv);
 
 /* ========== builtins.c ========== */
 void exitt(char **arv);
+void cd(char **arv);
 
 #endif
