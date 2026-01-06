623fb3c40ef2805b877fa90650c88590879ed89c
Aythann <aythancristovao@gmail.com>
2026-01-06 11:12:50 +0100

    Add main loop to read, parse, and execute commands

---
+++ shell.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * main - entry point of the simple shell
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	char *line;
+	size_t len;
+	ssize_t nread;
+	char **argv;
+	int interactive;
+
+	(void)ac;
+	(void)av;
+
+	line = NULL;
+	len = 0;
+	argv = NULL;
+	interactive = isatty(STDIN_FILENO);
+
+	while (1)
+	{
+		if (interactive != 0)
+			_puts("($) ");
+
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+			break;
+
+		argv = splitstring(line, " \n\t");
+		if (argv != NULL && argv[0] != NULL)
+			execute(argv);
+
+		freearv(argv);
+		argv = NULL;
+	}
+
+	if (interactive != 0)
+		_putchar('\n');
+
+	free(line);
+	return (0);
+}
